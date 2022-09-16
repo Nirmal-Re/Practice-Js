78e10c90806e65469806a7c47ae5698808c2c706
Nirmal Bhandari <nirmalbhandari@Nirmals-MBP.royalsurrey.nhs.uk>
2022-09-16 13:13:16 +0100

    SECOND command

--- Modal/script.js
+++ Modal/script.js
@@ -4,6 +4,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+//I am so bored
 //Opening modal
 const openModal = () => {
   modal.classList.remove('hidden');
